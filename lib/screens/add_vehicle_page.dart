9e5efc85c5b3e3f84f65d8749e36caf5e0bac956
cornegea <cornelisgea@gmail.com>
2025-04-24 14:01:44 +0700

    updt

--- lib/screens/add_vehicle_page.dart
+++ lib/screens/add_vehicle_page.dart
@@ -29,7 +29,7 @@ class AddVehiclePage extends StatelessWidget {
                 ),
                 SizedBox(width: 8),
                 Text(
-                  "Add ",
+                  "Add vehicle",
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
